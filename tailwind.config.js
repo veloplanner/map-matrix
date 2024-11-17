ada584cf1c1d4931424c5bb761c54e87a7e6ab61
Kacper Golinski <kacper.golinski@gmail.com>
2024-11-17 19:51:04 +0100

    add link to VeloPlanner
    One must remember about marketing :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#034732",
+      }
+    },
   },
   plugins: [],
 }
