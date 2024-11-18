f1c286416e9b9e61d6a566e2eb0cef592bd4dc6f
Kacper Golinski <kacper.golinski@gmail.com>
2024-11-18 12:54:35 +0100

    create ErrorBoundry

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import "maplibre-gl/dist/maplibre-gl.css";
 
 import { AppProvider } from "./contexts/AppContext";
 import { Layout } from "./components/Layout";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 export function App() {
   return (
     <AppProvider>
-      <Layout />
+      <ErrorBoundary>
+        <Layout />
+      </ErrorBoundary>
     </AppProvider>
   );
 }
